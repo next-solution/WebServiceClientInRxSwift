77d1cc67054b4bca2ce61b4d3dd23fdbcf32048a
Marcin Makurat <marcin.makurat@dgt.eu>
2020-09-13 21:42:43 +0200

    first commit

---
+++ WebServiceClientInRxSwift/Api.swift
@@ -0,0 +1,32 @@
+//
+//  ApiClient.swift
+//  WebServiceClientInRxSwift
+//
+//  Created by Marcin Makurat on 13/09/2020.
+//  Copyright © 2020 Marcin Makurat. All rights reserved.
+//
+
+import Foundation
+
+enum Api {}
+
+extension Api {
+    static func getCustomer() -> Endpoint<Customer> {
+        return Endpoint(path: "customer/profile")
+    }
+
+    static func patchCustomer(firstName: String, lastName: String) -> Endpoint<Customer> {
+        return Endpoint(
+            method: .patch,
+            path: "customer/profile",
+            parameters: ["firstName" : firstName,
+                         "lastName" : lastName]
+        )
+    }
+}
+
+
+//TODO Modify this
+final class Customer: Decodable {
+    let name: String
+}
---
+++ WebServiceClientInRxSwift/RestaurantViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  RestaurantViewModel.swift
+//  WebServiceClientInRxSwift
+//
+//  Created by Marcin Makurat on 13/09/2020.
+//  Copyright © 2020 Marcin Makurat. All rights reserved.
+//
+
+import Foundation
